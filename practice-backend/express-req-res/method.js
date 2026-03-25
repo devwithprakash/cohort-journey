bd87284652ce1a21986e7b1bc5a563c7514d8cb1
Prakash Jangid <prakashjangid7357@gmail.com>
2026-03-25 22:21:29 +0530

    feat/route methods and use middleware order

---
+++ practice-backend/express-req-res/method.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+const app = express();
+
+app.use("/api", (req, res, next) => {
+  console.log("Jate time milke jana");
+  next();
+});
+
+app
+  .route("/api/schedule")
+  .get((req, res) => {
+    res.end("Get");
+  })
+  .post((req, res) => {
+    res.end("Post");
+  });
+
+const server = app.listen(0, async () => {
+  const port = server.address().port;
+  const base = `http://127.0.0.1:${port}`;
+  try {
+    const res = await fetch(`${base}/api/schedule`);
+    const data = await res.text();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+  server.close(() => {
+    console.log("Server is running....");
+  });
+});
