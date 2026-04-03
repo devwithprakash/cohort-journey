3895fc444dbc9f36ec04c6bbf7d1f498e91f5489
Prakash Jangid <kanekiken8333@gmail.com>
2026-04-03 22:21:56 +0530

    drizzle and postgres setup complete

---
+++ practice-backend/typescript-auth/src/index.ts
@@ -0,0 +1,20 @@
+import http from "node:http";
+import 'dotenv/config';
+import { createApplication } from "./app";
+
+
+async function main() {
+  try {
+    const server = http.createServer(createApplication());
+    const PORT: number = Number(process.env.PORT) || 8080;
+
+    server.listen(PORT, () => {
+      console.log(`Http server is running on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error starting http server");
+    throw error;
+  }
+}
+
+main();
