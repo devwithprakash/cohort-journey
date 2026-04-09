a93a68a9acbe7f55a205b7cc75368a760011d7d3
Prakash Jangid <kanekiken8333@gmail.com>
2026-04-09 22:41:24 +0530

    db-design-04 add

--- assignments/book-my-show/src/app/auth/service.ts
+++ assignments/book-my-show/src/app/auth/service.ts
@@ -1,11 +1,11 @@
 
-interface RegsiterInput {
+interface RegisterInput {
     name: string;
     email: string;
     password: string;
     phone: string
 }
 
-const register = ({name, email, password, phone}: RegsiterInput)=>{
-
+const register = ({name, email, password, phone}: RegisterInput)=>{
+    
 }
